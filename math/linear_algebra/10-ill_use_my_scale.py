5fa39721f269a8ada63025186a0d0eabc358863b
kylaochweri <k.ochweri@alustudent.com>
2026-02-04 11:23:32 -0800

    linear algebra for ML

---
+++ math/linear_algebra/10-ill_use_my_scale.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+"""Creates a function that calculates the shape of an Ndarray"""
+
+
+def np_shape(matrix):
+    """Calculates the shape of an Ndarray"""
+    return tuple(matrix.shape)
+
